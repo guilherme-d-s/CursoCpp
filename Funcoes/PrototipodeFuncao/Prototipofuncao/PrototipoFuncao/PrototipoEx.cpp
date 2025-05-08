358c1cdf0c116685184105af4d00f069fff893d1
picolino <gds.z_@hotmail.com>
2025-05-08 19:12:22 -0300

    Adicionado Prototipo de Funcao

---
+++ Funcoes/PrototipodeFuncao/Prototipofuncao/PrototipoFuncao/PrototipoEx.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+float soma(float num1, float num2) {
+	return (num1 + num2);
+}
+int main()
+{
+
+	std::cout << soma(100, 500) << "\n";
+
+	system("PAUSE");
+	return 0;
+
+}
+
